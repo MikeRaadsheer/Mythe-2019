4571d8f1ea7ae8a65dcb203d9fb74cdb958d7dfc
Doppeltje <Doppeltje@users.noreply.github.com>
2019-03-07 09:25:20 +0100

    Movement & camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform target; // Drag the target to follow in the inspector.
+	public bool clamp;
+	public Vector2 clampX, clampY;
+
+	// Speed at which the camera follows the player.
+	private float smoothSpeed = 2f;
+
+	// Distance from player to camera.
+	private float yOffset = 3f;
+	private float zOffset = -5f;
+
+	private void Start()
+	{
+		// Rotate camera.
+		transform.eulerAngles = new Vector3(30, 0, 0);
+	}
+
+	private void FixedUpdate()
+	{
+		Follow();
+	}
+
+	private void Follow() // Handles camera distance.
+	{
+		Vector3 offset = new Vector3(0, yOffset, zOffset);
+		Vector3 desiredPos = target.position + offset;
+		Vector3 cameraPos = new Vector3(target.position.x, target.position.y + yOffset, target.position.z + zOffset);
+		Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
+
+		if (clamp)
+		{
+			transform.position = new Vector3(Mathf.Clamp(smoothedPos.x, clampX.x, clampY.y), Mathf.Clamp(smoothedPos.y, clampX.x, clampY.y), smoothedPos.z);
+		}
+		else
+		{
+			transform.position = cameraPos;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	// Movement.
+	public float moveSpeed;
+	//private float moveSpeed;
+	private float horizontalMove, verticalMove;
+	private bool diagonal;
+	private Vector3 movement;
+
+	// Components.
+	private Rigidbody rb;
+
+	// Animations.
+	private bool isIdle;
+
+	void Start() // Assign starting values.
+	{
+		// Components.
+		rb = GetComponent<Rigidbody>();
+
+		// Stats.
+	}
+
+	void Update()
+	{
+		PlayerMovement();
+	}
+
+	private void PlayerMovement() // Handles movement controls.
+	{
+		MovingDiagonal();
+
+		if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+		{
+			horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+			verticalMove = Input.GetAxisRaw("Vertical") * moveSpeed;
+			movement = new Vector3(horizontalMove, rb.velocity.y, verticalMove);
+			rb.velocity = Vector3.Lerp(rb.velocity, movement, 15 * Time.deltaTime);
+		}
+
+		if (Input.GetAxis("Horizontal") == 0 && (Input.GetAxis("Vertical") == 0))
+		{
+			Vector3 standStill = new Vector3(0, rb.velocity.y, 0);
+			rb.velocity = Vector3.Lerp(rb.velocity, standStill, 30 * Time.deltaTime);
+		}
+	}
+
+	private void MovingDiagonal() // Checks if player is walking on both the X and Z axis. If so, slows movement.
+	{
+		diagonal = horizontalMove != 0 && verticalMove != 0;
+		moveSpeed = !diagonal ? 5f : 3.5f;
+	}
+}
