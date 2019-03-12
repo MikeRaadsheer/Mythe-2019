f50cbc8ddd123566af1c5c0b32fa1ba60e5fdfff
sedzjad <stsheefe@gmail.com>
2019-03-12 13:42:45 +0100

    Added the inventory controls

---
+++ Assets/SedzjadInventory/InventoryScript/PlayerStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    public GameObject ItemManager;
+
+    public int AttackDamage;
+    public int Defence;
+
+    public Text AttackDamageText;
+    public Text DefenceText;
+
+    public void Start()
+    {
+        ItemManager = GameObject.FindWithTag("ItemManager");       
+
+    }
+
+    public void OnEquipItem()
+    {
+
+    }
+
+}
