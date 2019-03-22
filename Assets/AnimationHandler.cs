d9590817716d1a396912d8317afd565732574669
Ferri de Lange <ev.fdl.ve@gmail.com>
2019-03-22 10:48:19 +0100

    Added animations and imported level design

---
+++ Assets/AnimationHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour {
+    #region Public Fields
+    #endregion
+
+    #region Private Fields
+    #endregion
+
+    #region Unity Methods
+    void Start() {
+
+    }
+
+    void Update() {
+        if(GetComponent<Rigidbody>().velocity.x != 0) {
+            GetComponent<Animator>().SetInteger("AnimState", 1);
+        } else {
+            GetComponent<Animator>().SetInteger("AnimState", 0);
+        }
+
+        if(GetComponent<Rigidbody>().velocity.x > 0) {
+            GetComponent<SpriteRenderer>().flipX = false;
+        } else if(GetComponent<Rigidbody>().velocity.x < 0) {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
