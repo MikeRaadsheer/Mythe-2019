b54d77069d378b84448dd338b7d9a3392094d45b
Ferri de Lange <ev.fdl.ve@gmail.com>
2019-03-28 09:05:04 +0100

    Healthbar improved, fonts added

--- Assets/PlayerHealthUI.cs
+++ Assets/PlayerHealthUI.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerHealthUI : MonoBehaviour {
     #region Public Fields
     #endregion
 
     #region Private Fields
-    [SerializeField] private float health = 100;
+    [SerializeField] private float health;
+    private float maxHealth = 100;
     private Player player;
     private Image healthBar;
     [SerializeField] private Image healthBarBackground;
+    [SerializeField] private TextMeshProUGUI healthText;
 
     [SerializeField] private TextAsset playerJson;
     #endregion
@@ -23,20 +26,28 @@ public class PlayerHealthUI : MonoBehaviour {
     }
 
     void Start() {
-        healthBar.fillMethod = Image.FillMethod.Horizontal;
+        healthBar.fillMethod = Image.FillMethod.Horizontal; // Set fill method of the image to horizontal
         healthBar.fillOrigin = 0; // Fills image from the left
+
+        health = maxHealth;
     }
 
     void Update() {
-        health -= 0.1f;
+        health = Mathf.Clamp(health, 0, maxHealth);
+
+        string text = Mathf.Round(health).ToString() + " / " + maxHealth;
+        healthText.text = text;
 
         healthBar.fillAmount = Map(health, 0, 100, 0, 1); // Maps health from 0 and 100 to 0 and 1
-        healthBar.color = Color.Lerp(Color.red, Color.green, Map(health, 0, 100, 0, 1));
-        healthBarBackground.color = Color.Lerp(Color.red, Color.green, Map(health, 0, 100, 0, 1));
-
-        var color = healthBarBackground.color;
-        color.a = 0.3f;
-        healthBarBackground.color = color;
+        healthBar.color = Color.Lerp(Color.red, Color.green, Map(health, 0, maxHealth, 0, 1)); // Change the color of the healthbar based on the health amount
+        healthBarBackground.color = Color.Lerp(Color.red, Color.green, Map(health, 0, maxHealth, 0, 1));
+        
+        /*Get reference to the color and change the alpha*/ {
+            var color = healthBarBackground.color;
+            color.a = 0.3f;
+            healthBarBackground.color = color;
+        }
+        
     }
     #endregion
 
